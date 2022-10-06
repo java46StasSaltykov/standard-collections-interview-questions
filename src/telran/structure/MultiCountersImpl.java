4a9cafbef23519b58615ad61d0478ccc17632d47
StasSaltykov <stasyanych92@gmail.com>
2022-10-06 18:11:37 +0300

    HW #31 fix

--- src/telran/structure/MultiCountersImpl.java
+++ src/telran/structure/MultiCountersImpl.java
@@ -3,34 +3,54 @@ package telran.structure;
 import java.util.*;
 
 public class MultiCountersImpl implements MultiCounters {
+	
+	HashMap<Object, Integer> items = new HashMap<>(); // key - item, counter - counter
+	TreeMap<Integer, HashSet<Object>> counters = new TreeMap<>(); // key counter, counter - set of items having the key counter
 
-	HashMap<Object, Integer> items = new HashMap<>(); // key - item , value - counter
-	TreeMap<Integer, HashSet<Object>> counters = new TreeMap<>(); // key - counter, value - set of items having the key counter
-	
-	// consider using the map method computeIfAbscent()
-	
 	@Override
 	public Integer addItem(Object item) {
-		// TODO Auto-generated method stub
-		return null;
+		Integer res = items.merge(item, 1, Integer::sum);
+		if (res > 1) {
+			counterItemsRemove(res - 1, item);
+		}
+		counters.computeIfAbsent(res, e -> new HashSet<>()).add(item);
+		return res;
+	}
+
+	private void counterItemsRemove(int counter, Object item) {
+		HashSet<Object> set = counters.get(counter);
+		set.remove(item);
+		if (set.isEmpty()) {
+			counters.remove(counter);
+		}
 	}
 
 	@Override
 	public Integer getValue(Object item) {
-		// TODO Auto-generated method stub
-		return null;
+
+		return items.get(item);
 	}
 
 	@Override
 	public boolean remove(Object item) {
-		// TODO Auto-generated method stub
-		return false;
+		Integer counter = items.remove(item);
+		if (counter != null) {
+			counterItemsRemove(counter, item);
+		}
+		return counter != null;
 	}
 
 	@Override
 	public Set<Object> getMaxItems() {
-		// TODO Auto-generated method stub
-		return null;
+		Integer maxCounter;
+		Set<Object> res;
+		try {
+			maxCounter = counters.lastKey();
+			res = counters.get(maxCounter);
+		} catch (Exception e) {
+			res = Collections.emptySet();
+		}
+		return res;
 	}
 
 }
